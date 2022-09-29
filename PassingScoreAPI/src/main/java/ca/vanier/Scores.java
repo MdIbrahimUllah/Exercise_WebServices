0bd7c7bbad3ca5532f44547f58250090d79bc3c0
MarkBenedictMuyot <markmuyot@hotmail.com>
2022-09-29 17:53:16 -0400

    scores implementation

--- PassingScoreAPI/src/main/java/ca/vanier/Scores.java
+++ PassingScoreAPI/src/main/java/ca/vanier/Scores.java
@@ -1,3 +1,12 @@
+package ca.vanier;
+
 public class Scores {
+
+    void check(int score){
+        if (score<6) System.out.println("fail");
+        else
+        System.out.println("passed");
+
+    }
     
 }
