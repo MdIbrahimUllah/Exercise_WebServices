5e261b4c91fd3174199240f23ebc9ac1a4458cda
MdIbrahimUllah <jeasan0007@gmail.com>
2022-09-29 20:54:15 -0400

    Added updated scoreTest class

--- PassingScoreAPI/src/test/java/ca/vanier/ScoresTest.java
+++ PassingScoreAPI/src/test/java/ca/vanier/ScoresTest.java
@@ -2,12 +2,25 @@ package ca.vanier;
 
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
 
 import org.junit.Test;
 
 public class ScoresTest{
+    //when the case is true
     @Test
-    public void testResult(){
-    String args = "6";
+    public void testResultTrue(){
+    String args = "7";
+    boolean answer = Scores.result(args);
+    assertNotNull(answer);
+    assertTrue("Checking if scores grater than 6", answer == true);
+}
+
+@Test
+    public void testResultFalse(){
+    String args = "8";
+    boolean answer = Scores.result(args);
+    assertNotNull(answer);
+    assertFalse("Checking if scores less than 6", answer == false);
     }
 }
