fedb0a2ab50488699ead9ad38aa18bc3d07f8a9f
MdIbrahimUllah <jeasan0007@gmail.com>
2022-09-29 22:28:06 -0400

    added two more test cases in to ScoresTest class

--- PassingScoreAPI/src/test/java/ca/vanier/ScoresTest.java
+++ PassingScoreAPI/src/test/java/ca/vanier/ScoresTest.java
@@ -7,21 +7,39 @@ import static org.junit.Assert.assertFalse;
 import org.junit.Test;
 
 public class ScoresTest{
-    //when the case is true
+    //when the case is grater than 6
     @Test
-    public void testResultTrue(){
+    public void testResultTrue1(){
     String args = "7";
     boolean answer = Scores.result(args);
     assertNotNull(answer);
     assertTrue("Checking if scores grater than 6", answer == true);
 }
 
-//when the case is false
+//when the case is less than 6
 @Test
-    public void testResultFalse(){
+public void testResultTrue2(){
+String args = "5";
+boolean answer = Scores.result(args);
+assertNotNull(answer);
+assertTrue("Checking if scores less than 6", answer == false);
+}
+
+//when the case is grater than 6
+@Test
+    public void testResultFalse1(){
     String args = "8";
     boolean answer = Scores.result(args);
     assertNotNull(answer);
-    assertFalse("Checking if scores less than 6", answer == false);
+    assertFalse("Checking if scores grater than 6", answer == false);
     }
+
+    //when the case is less than 6
+@Test
+public void testResultFalse2(){
+String args = "8";
+boolean answer = Scores.result(args);
+assertNotNull(answer);
+assertFalse("Checking if scores less than 6", answer == false);
+}
 }
