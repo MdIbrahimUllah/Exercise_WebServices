474f1ca6bc7878ac9f79435522d3c72cf588affc
MdIbrahimUllah <jeasan0007@gmail.com>
2022-09-29 20:24:22 -0400

    adding Scores class

---
+++ PassingScoreAPI/src/test/java/ca/vanier/ScoresTest.java
@@ -0,0 +1,14 @@
+package ca.vanier;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class ScoresTest{
+    @Test
+    public void testResult(){
+    String args = "6";
+    
+    }
+}
