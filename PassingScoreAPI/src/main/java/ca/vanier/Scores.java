a083df8dc5f29fd0fac93a3bb0f660eefca103e9
MdIbrahimUllah <108957521+MdIbrahimUllah@users.noreply.github.com>
2022-09-29 20:04:44 -0400

    Updated Scores.java file

--- PassingScoreAPI/src/main/java/ca/vanier/Scores.java
+++ PassingScoreAPI/src/main/java/ca/vanier/Scores.java
@@ -1,17 +1,5 @@
 package ca.vanier;
-<<<<<<< HEAD
-=======
-
->>>>>>> e97e3b98d571c812592c284f74e964f8bf47987a
 public class Scores {
-
-    boolean check(int score){
-        if (score<6) return false;
-        else
-        return true;
-
-    }
-    
     public static boolean result(String args){
         double score = Double.parseDouble(args);
         return (score > 6);
